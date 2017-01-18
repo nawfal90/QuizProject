5c693c880f5f6532e1b493ac45746231d611e1ec
nawfal90 <nawfalksa@gmail.com>
2017-01-18 13:16:30 -0500

    remote file

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
-
+ 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    //I changed the questions here, because When I followed the book, I thought it is going to be something 
+    //I am not going to submit.
+    let questions: [String] = [
+        "What class is this one?",
+        "What is 9*9?",
+        "What is my name?"
+    ]
+    
+    let answers: [String] = [
+        "CSC-2310",
+        "81",
+        "Nawfal"
+    ]
+    var currentQuestionIndex: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = questions[currentQuestionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+           currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
-
-
+    
+    @IBAction func showAnswer(_ sender: UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
 }
 
